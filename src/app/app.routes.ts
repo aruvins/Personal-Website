f13514193ff48ed85c3bc41614e1056fe0d58342
aruvins <39351450+aruvins@users.noreply.github.com>
2024-08-13 12:58:50 -0400

    lazy load and update budget for deployment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: 'feature',
+      loadChildren: () => import('./app.module').then(m => m.WebcamModule)
+    }
+  ];
