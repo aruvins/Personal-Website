d2289fa9629326ea134e9bfdf68afe9d247657ac
aruvins <39351450+aruvins@users.noreply.github.com>
2024-08-17 15:04:22 -0400

    working facial tracking camera with age, gender, and emotions

--- facial-recognition-app/src/app/webcam/webcam.component.ts
+++ facial-recognition-app/src/app/webcam/webcam.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import * as faceapi from 'face-api.js';
 import * as tf from '@tensorflow/tfjs';
 
@@ -20,6 +20,8 @@ export class WebcamComponent implements AfterViewInit {
     await faceapi.nets.tinyFaceDetector.loadFromUri('/assets/models');
     await faceapi.nets.faceLandmark68Net.loadFromUri('/assets/models');
     await faceapi.nets.faceRecognitionNet.loadFromUri('/assets/models');
+    await faceapi.nets.faceExpressionNet.loadFromUri('/assets/models');
+    await faceapi.nets.ageGenderNet.loadFromUri('/assets/models');
     this.startVideo();
   }
 
@@ -30,7 +32,7 @@ export class WebcamComponent implements AfterViewInit {
         video.srcObject = stream;
         video.onloadedmetadata = () => {
           video.play();
-          this.onPlay(); // Call onPlay after video starts playing
+          this.onPlay();
         };
       })
       .catch(err => console.error('Error accessing webcam: ', err));
@@ -45,12 +47,22 @@ export class WebcamComponent implements AfterViewInit {
 
     setInterval(async () => {
       const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-        .withFaceLandmarks().withFaceDescriptors();
+        .withFaceLandmarks().withFaceDescriptors().withFaceExpressions().withAgeAndGender();
 
       const resizedDetections = faceapi.resizeResults(detections, displaySize);
       canvas.getContext('2d')?.clearRect(0, 0, canvas.width, canvas.height);
       faceapi.draw.drawDetections(canvas, resizedDetections);
       faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+
+      resizedDetections.forEach((detection: { detection?: any; age?: any; gender?: any; }) => {
+        const { age, gender } = detection;
+        const box = detection.detection.box;
+        const drawBox = new faceapi.draw.DrawBox(box, {
+          label: `${Math.round(age)} years old ${gender}`
+        });
+        drawBox.draw(canvas);
+      });
     }, 100);
   }
 }
